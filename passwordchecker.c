1e3daf73ed229b0e5bedf5bc22cb40c54ad095a0
Praveenm1910 <praveenm191004@gmail.com>
2025-01-06 09:48:54 -0500

    code header file added and modified

--- passwordchecker.c
+++ passwordchecker.c
@@ -3,7 +3,14 @@
 int main() {
     printf("--------------------------------------------------------------------------------------\n");
     printf("Welcome to my Password Strength Checker Tool\n");
-    printf("This tool reviews and returns your password strength (1 to 10).\n");
+    printf("This is a basic tool that helps you to know the strength of your passwords.\n");
+    printf("This tool will review and return your password strength in points ranging from 1 to 10.\n");
+    printf("This tool will analyze the strength of your password based on the following:\n");
+    printf("1. Your password length should be at least 8 characters long: 1 point.\n");
+    printf("2. Password must contain at least one uppercase letter: 1 point.\n");
+    printf("3. If the password has three or more lowercase letters: 4 points.\n");
+    printf("4. If the password contains at least one special symbol: 1 point.\n"); 
+    printf("5. If the password contains 3 or more digits that are not sequential: 3 points.\n");
     printf("--------------------------------------------------------------------------------------\n");
 
     // Get the input from the user securely 
